def73005ea2b4e0cd2067a3b3721fc5fed240579
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-05-09 03:47:25 +0000

    hi

--- config/dashboard.ts
+++ config/dashboard.ts
@@ -13,20 +13,6 @@ export const dashboardConfig: DashboardConfig = {
     },
   ],
   sidebarNav: [
-    {
-      title: "Posts",
-      href: "/dashboard",
-      icon: "post",
-    },
-    {
-      title: "Billing",
-      href: "/dashboard/billing",
-      icon: "billing",
-    },
-    {
-      title: "Settings",
-      href: "/dashboard/settings",
-      icon: "settings",
-    },
+    
   ],
 }
